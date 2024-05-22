041a120a57af2c8816c3dab61b3c5f7c08538dce
unknown <agutierr22jaumebalmes.net>
2024-05-22 15:10:19 +0200

    Themes

---
+++ JoyClinic/frontEndApp/themes/theme.js
@@ -0,0 +1,18 @@
+const theme= {
+    light: {
+        theme: 'light',
+        color: 'black',
+        background: 'white',
+        lineColor: 'black',
+        calendarTodayTextColor: '#00adf5'
+    },
+    dark: {
+        theme: 'dark',
+        color: 'white',
+        background: 'black',
+        lineColor: 'white',
+        calendarTodayTextColor: '#1E90FF'
+    }
+}
+
+export default theme;
